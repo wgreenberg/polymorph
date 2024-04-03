ac1cad23e8a75d892ca95403dd4b719127921787
Jasper St. Pierre <jstpierre@mecheye.net>
2024-04-02 18:55:35 -0700

    blte decoding

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+
+#![allow(unused, dead_code)]
+
 use std::{collections::HashMap, io::SeekFrom, path::{Path, PathBuf}};
 
 use deku::{bitvec::BitSlice, DekuContainerRead, DekuError, DekuRead};
 use thiserror::Error;
 use tokio::{fs, io::{AsyncReadExt, AsyncSeekExt}};
+
+use miniz_oxide::inflate::{decompress_to_vec_zlib, DecompressError};
 
 const PATCH_SERVER: &str = "http://us.patch.battle.net:1119";
 const PRODUCT: &str = "wow_classic";
@@ -18,6 +23,8 @@ enum Error {
     DekuError(#[from] DekuError),
     #[error("Missing CKey")]
     MissingCKey,
+    #[error("Invalid Zlib")]
+    ZlibError(DecompressError),
 }
 
 struct Manifest {
@@ -224,8 +231,46 @@ impl ArchiveIndex {
     }
 }
 
-fn decode_btle(buf: &[u8]) -> Result<Vec<u8>, Error> {
-    todo!();
+#[derive(DekuRead, Debug)]
+struct BLTEChunk {
+    #[deku(endian = "big")]
+    pub compressed_size: u32,
+    #[deku(endian = "big")]
+    pub uncompressed_size: u32,
+    #[deku(endian = "big")]
+    pub checksum: [u8; 0x10],
+}
+
+#[derive(DekuRead, Debug)]
+#[deku(magic = b"BLTE")]
+struct BLTEHeader {
+    #[deku(endian = "big")]
+    pub data_offset: u32,
+    pub flag: u8,
+    #[deku(endian = "big", bytes = 3)]
+    pub chunk_count: u32,
+    #[deku(count = "chunk_count")]
+    pub chunks: Vec<BLTEChunk>,
+}
+
+fn decode_blte(buf: &[u8]) -> Result<Vec<u8>, Error> {
+    let header = BLTEHeader::from_bytes((buf, 0))?.1;
+    let mut out = Vec::new();
+
+    let mut data_offs = header.data_offset as usize;
+    for chunk in &header.chunks {
+        let chunk_buf = &buf[data_offs .. data_offs + (chunk.compressed_size as usize)];
+        let frame_type = chunk_buf[0] as char;
+        let chunk_data = &chunk_buf[1 .. chunk_buf.len()];
+        match frame_type {
+            'N' => out.extend(chunk_data),
+            'Z' => out.extend(decompress_to_vec_zlib(chunk_data).map_err(Error::ZlibError)?),
+            c => panic!("{}", c),
+        }
+        data_offs += chunk.compressed_size as usize;
+    }
+
+    Ok(out)
 }
 
 struct RootFile {
@@ -355,11 +400,21 @@ async fn main() {
 
 #[cfg(test)]
 mod tests {
+    use std::{fs::File, io::Read};
+
     use super::*;
 
     #[test]
     fn test_hexstring() {
         let hex: [u8; 4] = [0x13, 0x12, 0xde, 0xad];
         assert_eq!(hexstring(&hex), "1312dead");
+    }
+
+    #[test]
+    fn test_blte_decode() {
+        let test_file = std::fs::read("./test/test1.blte.out").unwrap();
+
+        let buf = decode_blte(&test_file).unwrap();
+        dbg!(buf);
     }
 }
