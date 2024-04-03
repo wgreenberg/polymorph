a407580862b6a3692d365e3b4cdd839727f84ab3
Will Greenberg <willg@eff.org>
2024-04-03 11:21:30 -0700

    use read_exact to prevent underreads

--- src/util.rs
+++ src/util.rs
@@ -8,7 +8,7 @@ pub async fn fetch_data_fragment<P: AsRef<Path>>(path: P, offset: usize, size: u
     let mut file = fs::File::open(path).await?;
     file.seek(SeekFrom::Start(offset as u64)).await?;
     let mut buf = vec![0; size];
-    file.read(&mut buf).await?;
+    file.read_exact(&mut buf).await?;
     Ok(buf)
 }
 
